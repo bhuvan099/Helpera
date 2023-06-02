f0bed388384003ab67d5de3e029cc642b1ac63f6
bhuvan099 <pbhuvan99@gmail.com>
2023-06-02 22:30:17 +0530

    updated organization page

---
+++ src/Components/Organizers/Odefault.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./Odefault.module.css";
+
+const Odefault = (props) => {
+  return (
+    <>
+      <div className={classes.maincls2}></div>
+    </>
+  );
+};
+
+export default Odefault;
---
+++ src/Components/Organizers/Org_info.js
@@ -0,0 +1,14 @@
+import React from "react";
+import classes from "./Org_info.module.css";
+
+const Org_info = (props) => {
+  return (
+    <>
+      <div className={classes.maincls1}>
+        <h1>Organization info </h1>
+      </div>
+    </>
+  );
+};
+
+export default Org_info;
