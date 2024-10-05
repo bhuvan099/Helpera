eae35c902d57bbe9880585eec7f7a2097d0f4d50
bhuvan099 <pbhuvan99@gmail.com>
2024-10-05 18:40:18 +0530

    first pull

--- src/Components/Organizers/Organizer.js
+++ src/Components/Organizers/Organizer.js
@@ -20,27 +20,33 @@ const Organizer = () => {
   //const [t2, istoggle2] = useState(false);
   console.log(toggle.t1);
 
-  return <>
-  {user ?<div className={classes.main}>
-      <div>
-        <Sidea user={user} />
-      </div>
-      <div className={classes.in}></div>
-      <div>
-        <Sideb settoggle={istoggle} user={user} />
-      </div>
-      <div>
-        {toggle.t1 && <Org_info user={user} /> }
-        {toggle.t2 && <Sidec camp={user.campaigns} /> }
-        {toggle.t3 && <Volenteer_info />}
-        {toggle.t4 && <Announcements />}
-        {/* {toggle.t2 && <Odefault />} */}
-        {/* <Sidec /> */}
-      </div>
-    </div>:
-   <div className={classes.spin}>
-     <div className={classes.loader}></div> <p>Loading Profile Details....</p>
-     </div>}
+  return (
+    <>
+      {user ? (
+        <div className={classes.main}>
+          <div>
+            <Sidea user={user} />
+          </div>
+          <div className={classes.in}></div>
+          <div>
+            <Sideb settoggle={istoggle} user={user} />
+          </div>
+          <div>
+            {toggle.t1 && <Org_info user={user} />}
+            {toggle.t2 && <Sidec camp={user.campaigns} />}
+            {toggle.t3 && <Volenteer_info />}
+            {toggle.t4 && <Announcements />}
+            {/* {toggle.t2 && <Odefault />} */}
+            {/* <Sidec /> */}
+          </div>
+        </div>
+      ) : (
+        <div className={classes.spin}>
+          <div className={classes.loader}></div>{" "}
+          <p>Loading Profile Details....</p>
+        </div>
+      )}
     </>
+  );
 };
 export default Organizer;
